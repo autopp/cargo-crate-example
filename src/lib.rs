85135a223684909d68a920ed94e202c8d0d82f80
autopp <autopp.inc@gmail.com>
2024-09-07 15:39:52 +0900

    feat: add inc()

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod add;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn inc(x: i64) -> i64 {
+    add::add(x, 1)
 }
