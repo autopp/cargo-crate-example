5829361abc5a0b932b52734182ffc3b4ae025e74
autopp <autopp.inc@gmail.com>
2024-10-07 23:07:54 +0900

    chore: mv .gitignore

--- autopp_inc/src/add.rs
+++ autopp_inc/src/add.rs
@@ -1,3 +1,11 @@
 pub fn add(left: i64, right: i64) -> i64 {
     left + right
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add() {
+        assert_eq!(3, super::add(1, 2));
+    }
+}
--- target/debug/deps/autopp_inc-ca7df9ddcabbe97a.d
+++
@@ -1,6 +0,0 @@
-/Users/autopp/ghq/github.com/autopp/cargo-crate-example/target/debug/deps/autopp_inc-ca7df9ddcabbe97a: autopp_inc/src/lib.rs autopp_inc/src/add.rs
-
-/Users/autopp/ghq/github.com/autopp/cargo-crate-example/target/debug/deps/autopp_inc-ca7df9ddcabbe97a.d: autopp_inc/src/lib.rs autopp_inc/src/add.rs
-
-autopp_inc/src/lib.rs:
-autopp_inc/src/add.rs:
